4ee4e1198088de1fc676c596ca40bf3d9df57c5f
Agustina <agustinagauna@gmail.com>
2020-05-21 09:51:44 -0300

    Initial commit

---
+++ src/main/java/ar/com/ada/tp/TpApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ada.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpApplication.class, args);
+	}
+
+}
